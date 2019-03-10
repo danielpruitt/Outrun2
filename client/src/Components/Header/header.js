f5e25d3eaf231180142b00e92e24970ba0297189
Daniel <danielpruitt6@gmail.com>
2019-03-10 11:22:01 -0400

    new carousel and navbar, added features to both

--- client/src/Components/Header/header.js
+++ client/src/Components/Header/header.js
@@ -8,23 +8,22 @@ import Parallax from '../Parallax/Parallax';
 
 const Header = () => (
     // <header className='header responsive-img'>
-    <header className="">
+    <header className="hide-on-med-and-down">
         {/* this is a container built exactly like a regular container just with another class hard coded inside */}
 
-        {/* <img className="responsive-img" src={require("../../images/headerlogo.png")} alt="HeaderLogo"/> */}
         {/* <Parallax image="https://lh3.googleusercontent.com/yvYRX28GPpKlZo8uc40aMlQrbVx1vWpPSf9uHBPgvOtEU0oQtfmucQvJvZGtPp_V8nBfttBXbl_LSx67lSr54LY27v_nbFL1oHLdl16vXrjfYuXxUFIuLaOy1QHUtEBRaVg4krYRryuVkN2CvM4yT2f4w2YPxA7klDE-NlyhIWh107zopYq3QNKCgtn3bC5lapZ2XWxn2sZ6Pa7xcWF9ut6TZyxrT0l-D8HbvXxElECjpNaUd0T1pmuvigru5CC-oZB0blADfGKWyrn72bq8Oe_3fRd0j4PbFxG-34lpNZkpw3rDbuiMkqwMSmbD57A6k0hog9nX_VK5BoNV4KdzTE5GonK5PffQ3RxEL99x1ogXn1kLSpXqmlFXQ8ybpFTxSDsHROB3JLlMS-xxmauTLCt3Int0NxIAE8VWiEM2Ine4BksKhSrORNGgmUyPTCa5XZmNiUDjU-H2ArxV9LyaUVIVT7GPnU4twhvPSFzm9g-WTlXLLN8pU1aAojAAO9ulmqEEkUFfUoGs0Mp6ahpPlFS5maHtSnvvvXsdYzpIF03WI01pxiA17lvJLLov0RqHtMFdaVTXJjwhsrdjLntYvQPBdOoW9habW1owa8du51Tu3fa4rma_obE9nErVET11OxmHmcYNcaetwrq3vJ6iDEO7r9hE6WQ=w1270-h914-no"/> */}
         {/* <img className="respImg" src="https://lh3.googleusercontent.com/yvYRX28GPpKlZo8uc40aMlQrbVx1vWpPSf9uHBPgvOtEU0oQtfmucQvJvZGtPp_V8nBfttBXbl_LSx67lSr54LY27v_nbFL1oHLdl16vXrjfYuXxUFIuLaOy1QHUtEBRaVg4krYRryuVkN2CvM4yT2f4w2YPxA7klDE-NlyhIWh107zopYq3QNKCgtn3bC5lapZ2XWxn2sZ6Pa7xcWF9ut6TZyxrT0l-D8HbvXxElECjpNaUd0T1pmuvigru5CC-oZB0blADfGKWyrn72bq8Oe_3fRd0j4PbFxG-34lpNZkpw3rDbuiMkqwMSmbD57A6k0hog9nX_VK5BoNV4KdzTE5GonK5PffQ3RxEL99x1ogXn1kLSpXqmlFXQ8ybpFTxSDsHROB3JLlMS-xxmauTLCt3Int0NxIAE8VWiEM2Ine4BksKhSrORNGgmUyPTCa5XZmNiUDjU-H2ArxV9LyaUVIVT7GPnU4twhvPSFzm9g-WTlXLLN8pU1aAojAAO9ulmqEEkUFfUoGs0Mp6ahpPlFS5maHtSnvvvXsdYzpIF03WI01pxiA17lvJLLov0RqHtMFdaVTXJjwhsrdjLntYvQPBdOoW9habW1owa8du51Tu3fa4rma_obE9nErVET11OxmHmcYNcaetwrq3vJ6iDEO7r9hE6WQ=w1270-h914-no"/> */}
+            <img src={require("../../images/neonSign.jpg")} className="respImg" alt="HeaderLogo"/>
 
         <HeaderContainer>
-            {/* <img src={require("../../images/headerlogo.png")} alt="HeaderLogo"/> */}
-            <img className="respImg" src="https://lh3.googleusercontent.com/yvYRX28GPpKlZo8uc40aMlQrbVx1vWpPSf9uHBPgvOtEU0oQtfmucQvJvZGtPp_V8nBfttBXbl_LSx67lSr54LY27v_nbFL1oHLdl16vXrjfYuXxUFIuLaOy1QHUtEBRaVg4krYRryuVkN2CvM4yT2f4w2YPxA7klDE-NlyhIWh107zopYq3QNKCgtn3bC5lapZ2XWxn2sZ6Pa7xcWF9ut6TZyxrT0l-D8HbvXxElECjpNaUd0T1pmuvigru5CC-oZB0blADfGKWyrn72bq8Oe_3fRd0j4PbFxG-34lpNZkpw3rDbuiMkqwMSmbD57A6k0hog9nX_VK5BoNV4KdzTE5GonK5PffQ3RxEL99x1ogXn1kLSpXqmlFXQ8ybpFTxSDsHROB3JLlMS-xxmauTLCt3Int0NxIAE8VWiEM2Ine4BksKhSrORNGgmUyPTCa5XZmNiUDjU-H2ArxV9LyaUVIVT7GPnU4twhvPSFzm9g-WTlXLLN8pU1aAojAAO9ulmqEEkUFfUoGs0Mp6ahpPlFS5maHtSnvvvXsdYzpIF03WI01pxiA17lvJLLov0RqHtMFdaVTXJjwhsrdjLntYvQPBdOoW9habW1owa8du51Tu3fa4rma_obE9nErVET11OxmHmcYNcaetwrq3vJ6iDEO7r9hE6WQ=w1270-h914-no" alt="HeaderLogo"/>
+            {/* <img className="respImg" src="https://lh3.googleusercontent.com/yvYRX28GPpKlZo8uc40aMlQrbVx1vWpPSf9uHBPgvOtEU0oQtfmucQvJvZGtPp_V8nBfttBXbl_LSx67lSr54LY27v_nbFL1oHLdl16vXrjfYuXxUFIuLaOy1QHUtEBRaVg4krYRryuVkN2CvM4yT2f4w2YPxA7klDE-NlyhIWh107zopYq3QNKCgtn3bC5lapZ2XWxn2sZ6Pa7xcWF9ut6TZyxrT0l-D8HbvXxElECjpNaUd0T1pmuvigru5CC-oZB0blADfGKWyrn72bq8Oe_3fRd0j4PbFxG-34lpNZkpw3rDbuiMkqwMSmbD57A6k0hog9nX_VK5BoNV4KdzTE5GonK5PffQ3RxEL99x1ogXn1kLSpXqmlFXQ8ybpFTxSDsHROB3JLlMS-xxmauTLCt3Int0NxIAE8VWiEM2Ine4BksKhSrORNGgmUyPTCa5XZmNiUDjU-H2ArxV9LyaUVIVT7GPnU4twhvPSFzm9g-WTlXLLN8pU1aAojAAO9ulmqEEkUFfUoGs0Mp6ahpPlFS5maHtSnvvvXsdYzpIF03WI01pxiA17lvJLLov0RqHtMFdaVTXJjwhsrdjLntYvQPBdOoW9habW1owa8du51Tu3fa4rma_obE9nErVET11OxmHmcYNcaetwrq3vJ6iDEO7r9hE6WQ=w1270-h914-no" alt="HeaderLogo"/> */}
 
             <Row>
                 <Col size='s3'>
-                    <p className='beersContent neon'>Beers</p>
+                    <a className='beersContent neon' href="#ourbeers">Beers</a>
                 </Col>
                 <Col size='s3'>
-                    <p className='ourStory neon right-align'>Our Story</p>
+                    <a className='ourStory neon right-align' href="#ourstory">Our Story</a>
                 </Col>
             </Row>
 
--- client/src/pages/Landing.js
+++ client/src/pages/Landing.js
@@ -9,7 +9,7 @@ import Navbar from "../Components/Navbar/Navbar";
 // import RevealCard from "../Components/Card/Card";
 import API from "../utils/API";
 import Header from "../Components/Header/header";
-import CustomCard from  "../Components/Card/CustomCard";
+import CustomCard from "../Components/Card/CustomCard";
 import { Link } from "react-router-dom";
 
 // trying out this image card instead of a dynamic card
@@ -64,28 +64,30 @@ class Landing extends Component {
 
                 <Header />
                 {/* Our Beers */}
-                <Navbar/>
+                
+
                 <Section>
+                    <Navbar />
                     <Container>
                         <Row>
                             <Col size="s12">
-                                <h3 className="lazer84 border-bottom">Beers</h3>
+                                <h3 className="lazer84 border-bottom scrollspy" id="ourbeers">Beers</h3>
                             </Col>
                         </Row>
                         <Row>
                             {this.state.allBeers.map(beer => (
                                 <div key={beer._id}>
                                     <Col size="l4 m6 s12">
-                                    
-                                    <CustomCard
-                                        image={beer.image}
-                                        beerName={beer.name}
-                                        details={beer.details}
-                                        abv={beer.abv}
-                                        type={beer.type}
-                                        ibu={beer.ibu}
-                                        id={beer._id}
-                                    />
+
+                                        <CustomCard
+                                            image={beer.image}
+                                            beerName={beer.name}
+                                            details={beer.details}
+                                            abv={beer.abv}
+                                            type={beer.type}
+                                            ibu={beer.ibu}
+                                            id={beer._id}
+                                        />
 
                                     </Col>
                                 </div>
@@ -96,7 +98,7 @@ class Landing extends Component {
                 </Section>
 
                 <Parallax
-                    image={require("../images/OBC_NeonSign_Logotype.jpg")}
+                    image={require("../images/bottling.jpg")}
                 />
 
                 {/* Our Story */}
@@ -104,14 +106,14 @@ class Landing extends Component {
                     <Container>
                         <Row>
                             <Col size="s12">
-                                <h3 className="lazer84 border-bottom">Our Story</h3>
+                                <h3 className="lazer84 border-bottom scrollspy" id="ourstory">Our Story</h3>
                             </Col>
                         </Row>
                         <Row>
                             <Col size="s12">
                                 <div>
-                                <p>We are a brewer owned and operated small-scale production brewery that focuses on approachable and highly drinkable small-batch brews. We put our own spin on a variety of styles, old and new. Our beer is brewed in house and only available in our taproom. We take our beer seriously, but not ourselves, and are dedicated to brewing rarities, untasted originals, and re-imagined favorites. Remember to #followtheneon</p>
-                        
+                                    <p>We are a brewer owned and operated small-scale production brewery that focuses on approachable and highly drinkable small-batch brews. We put our own spin on a variety of styles, old and new. Our beer is brewed in house and only available in our taproom. We take our beer seriously, but not ourselves, and are dedicated to brewing rarities, untasted originals, and re-imagined favorites. Remember to #followtheneon</p>
+
                                 </div>
                             </Col>
                         </Row>
@@ -125,12 +127,12 @@ class Landing extends Component {
                     </Container>
                 </Section>
 
-                <Parallax
+                {/* <Parallax
                     image={require("../images/brewRoom.jpg")}
-                />
+                /> */}
 
                 {/* Location: need address and maybe add in google maps */}
-                <Section>
+                {/* <Section>
                     <Container>
                         <Row>
                             <Col size="s12">
@@ -147,14 +149,14 @@ class Landing extends Component {
                             </Col>
                         </Row>
                     </Container>
-                </Section>
+                </Section> */}
 
-                <Parallax
+                {/* <Parallax
                     image={require("../images/backroom.jpeg")}
-                />
+                /> */}
 
                 {/* Contact */}
-                <Section>
+                {/* <Section>
                     <Container>
                         <Row>
                             <Col size="s12">
@@ -166,14 +168,14 @@ class Landing extends Component {
                                 <div>
                                     <p>
                                         In volutpat, augue quis accumsan fringilla, elit metus fermentum justo, nec commodo metus risus sed nunc. Proin id congue nisi. Nulla tempus ipsum at pharetra dignissim. Proin dapibus, dolor ac venenatis pulvinar, est odio finibus mi, at tincidunt metus justo a felis. Praesent vulputate turpis id orci vehicula, id fringilla massa maximus. Mauris eleifend elit nulla, vel elementum arcu tincidunt non. Etiam sed feugiat odio. Quisque at facilisis mauris. Sed id velit efficitur, cursus eros iaculis, dapibus dui. Phasellus malesuada tellus at maximus pretium.
-    
+
                                         Quisque condimentum nisi eu faucibus ornare. Curabitur elit sem, fringilla ac lacus in, dapibus vehicula lectus. Morbi vulputate maximus dolor eget semper. Etiam semper grfavida sagittis. Ut eget elit sed metus dictum dapibus. Quisque scelerisque nisl tristique, fringilla lacus fringilla, posuere sapien. Maecenas eget purus in dui porta efficitur. Nulla nec urna sapien.
                                     </p>
                                 </div>
                             </Col>
                         </Row>
                     </Container>
-                </Section>
+                </Section> */}
 
                 <Parallax
                     image={require("../images/brewRoom.jpg")}
